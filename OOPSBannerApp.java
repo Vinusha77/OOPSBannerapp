31ce67702b379833efc49f663505d8f97386631d
Vinusha77 <desagiri.vinusha@gmail.com>
2026-03-03 15:31:10 +0530

    UC2 Updated banner using string concatenation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,16 @@
-/**
- * OOPS Banner App
- * UC2: Print formatted banner
- */
-
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("====================");
-        System.out.println("     OOPS APP       ");
-        System.out.println("====================");
+
+        String line1 = "*****  *****  *****  *****";
+        String line2 = "*   *  *   *  *   *  *     ";
+        String line3 = "*****  *****  *****  *****";
+        String line4 = "*   *  *      *      *   *";
+        String line5 = "*****  *      *      *****";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
